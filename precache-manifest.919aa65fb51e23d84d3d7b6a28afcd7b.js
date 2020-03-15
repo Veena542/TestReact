55b3896533eb30a139c753aefcda09d5bc846c1e
Veena542 <62172657+Veena542@users.noreply.github.com>
2020-03-15 11:52:18 +0530

    Updates

---
+++ precache-manifest.919aa65fb51e23d84d3d7b6a28afcd7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10dd2299e2e7916cb564c6b6568c0e1",
+    "url": "/TestReact/index.html"
+  },
+  {
+    "revision": "9e3fa6c8cb6aad7f5517",
+    "url": "/TestReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e222fe10355bd1a37333",
+    "url": "/TestReact/static/js/2.6fc7da52.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/TestReact/static/js/2.6fc7da52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e3fa6c8cb6aad7f5517",
+    "url": "/TestReact/static/js/main.9eaa2b91.chunk.js"
+  },
+  {
+    "revision": "206ba86c6d5d3184cfb2",
+    "url": "/TestReact/static/js/runtime-main.bbb01f7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TestReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
